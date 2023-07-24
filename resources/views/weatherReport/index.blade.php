78e48f5d21e839fbbae561013aeff936b0433b5b
janmitha bangera <janmithabangera@gmail.com>
2023-07-24 21:42:29 +0530

    index blade change

--- resources/views/weatherReport/index.blade.php
+++ resources/views/weatherReport/index.blade.php
@@ -5,7 +5,6 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
